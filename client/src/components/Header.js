aebaed4ef0801c12634d631bb244546483da792b
sarang <sarang.office01@gmail.com>
2023-03-19 11:40:51 +0530

    add redux toolkit for state management

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import {Box,AppBar,Toolbar,Button, Typography, Tabs, Tab} from '@mui/material'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
     const [value,setValue]=useState()
+    const isLogin=useSelector((state)=>state.auth.isLogin)
+    
   return (
     <>
         <AppBar position='sticky'>
@@ -11,16 +14,32 @@ const Header = () => {
                 <Typography variant='h4' flex={1}>
                     Blog App
                 </Typography>
-                <Box display={'flex'} marginLeft='auto'  >
-                    <Tabs textColor='inherit' value={value} onChange={(e,val)=>setValue(val)}>
-                        <Tab label='Blogs' LinkComponent={Link} to='/blogs'/>
-                        <Tab label='My Blogs'  LinkComponent={Link} to='/my-blogs'/>
-                    </Tabs>
-                </Box>
+               {
+                isLogin && 
+                (
+                     <Box display={'flex'} marginLeft='auto'  >
+                        <Tabs textColor='inherit' value={value} onChange={(e,val)=>setValue(val)}>
+                            <Tab label='Blogs' LinkComponent={Link} to='/blogs'/>
+                            <Tab label='My Blogs'  LinkComponent={Link} to='/my-blogs'/>
+                        </Tabs>
+                    </Box>
+                )
+               }
                 <Box display={'flex'} marginLeft='auto'>
-                    <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to='/login'>Login</Button>
-                    <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to='/register'>Register</Button>
-                    <Button sx={{margin:1,color:'white'}} >Logout</Button>
+                    {
+                        !isLogin && (
+                            <>
+                                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to='/login'>Login</Button>
+                                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to='/register'>Register</Button>
+                            </>
+                        )
+                    }
+                    
+                    {
+                        isLogin && (
+                            <Button sx={{margin:1,color:'white'}} >Logout</Button>
+                        )
+                    }
                 </Box>
             </Toolbar>
         </AppBar>
---
+++ client/src/redux/authSlice.js
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const authSlice=createSlice({
+    name:'auth',
+    initialState:{
+        isLogin:false,
+    },
+    reducers:{
+        login:(state)=>{
+            state.isLogin=true
+        },
+        logout:(state)=>{
+            state.isLogin=false
+        }
+    }
+})
+
+export const {login,logout}=authSlice.actions
