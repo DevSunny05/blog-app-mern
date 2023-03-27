e77ab9ca1fb173b3a567eb37f7211de678a2aee8
sarang <sarang.office01@gmail.com>
2023-03-27 21:01:18 +0530

    make some changes

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
+import CardHeader from '@mui/material/CardHeader'
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
+import CardContent from '@mui/material/CardContent'
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
